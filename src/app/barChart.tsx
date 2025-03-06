a742d7578e13f2adbc2f730eb3faffd1807d7379
Bharat <bharatramesh2601@gmail.com>
2025-03-05 22:41:15 -0500

    added the button

---
+++ src/app/barChart.tsx
@@ -0,0 +1,9 @@
+const BarChart = () => {
+    return (
+        <div>
+            Chart would be here...
+        </div>
+    )
+}
+
+export default BarChart;
---
+++ src/app/data.tsx
@@ -0,0 +1,11 @@
+const CHART_DATA = [
+    { id: "dep-1", name: "Legal", ticketCount: 32, colour: "#3F888F" },
+    { id: "dep-2", name: "Sales", ticketCount: 20, colour: "#1D1E33" },
+    { id: "dep-3", name: "Engineering", ticketCount: 60, colour: "#E1CC4F" },
+    { id: "dep-4", name: "Manufacturing", ticketCount: 5, colour: "#FFA420" },
+    { id: "dep-5", name: "Maintenance", ticketCount: 14, colour: "#D4R2SW" },
+    { id: "dep-6", name: "Human Resources", ticketCount: 35, colour: "#3F888F" },
+    { id: "dep-7", name: "Events", ticketCount: 43, colour: "#3F888F" }
+];
+
+export default CHART_DATA;
