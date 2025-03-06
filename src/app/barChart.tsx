b75ea105502d44dd8d259c82c5fc69fb0404ed22
Bharat <bharatramesh2601@gmail.com>
2025-03-05 23:17:07 -0500

    working chart

--- src/app/barChart.tsx
+++ src/app/barChart.tsx
@@ -1,7 +1,40 @@
+import { useMemo } from "react"
+
+const Bar = ({
+    name,
+    color,
+    ticketCount,
+    height
+}) => {
+    return (
+        <div 
+        className="bar"
+        style={{
+            height: `${height}%`,
+            // height: `${ticketCount}%`,
+            backgroundColor: color
+        }}
+        />
+    )
+}
+
 const BarChart = ({ data }) => {
+    const maxTicketCount = useMemo(() => {
+        return Math.max(...data.map(item => item.ticketCount));
+    }, [])
     return (
         <div className="chart-container">
-            <div className="chart"></div>
+            <div className="chart">
+                {
+                    data.map(item => {
+                        return <Bar 
+                        key={item.id}
+                        // 30 / 60 * 100 = 50
+                        height={item.ticketCount / maxTicketCount * 100}
+                        {...item}/>
+                    })
+                }
+            </div>
             <div className="y-axis-label">No. of Tickets</div>
             <div className="x-axis-label">Departments</div>
         </div>
--- src/app/data.tsx
+++ src/app/data.tsx
@@ -1,11 +1,11 @@
 const CHART_DATA = [
-    { id: "dep-1", name: "Legal", ticketCount: 32, colour: "#3F888F" },
-    { id: "dep-2", name: "Sales", ticketCount: 20, colour: "#1D1E33" },
-    { id: "dep-3", name: "Engineering", ticketCount: 60, colour: "#E1CC4F" },
-    { id: "dep-4", name: "Manufacturing", ticketCount: 5, colour: "#FFA420" },
-    { id: "dep-5", name: "Maintenance", ticketCount: 14, colour: "#D4R2SW" },
-    { id: "dep-6", name: "Human Resources", ticketCount: 35, colour: "#3F888F" },
-    { id: "dep-7", name: "Events", ticketCount: 43, colour: "#3F888F" }
+    { id: "dep-1", name: "Legal", ticketCount: 32, color: "#3F888F" },
+    { id: "dep-2", name: "Sales", ticketCount: 20, color: "#1D1E33" },
+    { id: "dep-3", name: "Engineering", ticketCount: 60, color: "#E1CC4F" },
+    { id: "dep-4", name: "Manufacturing", ticketCount: 5, color: "#FFA420" },
+    { id: "dep-5", name: "Maintenance", ticketCount: 14, color: "#da3036" },
+    { id: "dep-6", name: "Human Resources", ticketCount: 35, color: "#46a758" },
+    { id: "dep-7", name: "Events", ticketCount: 43, color: "#52a9ff" }
 ];
 
 export default CHART_DATA;
